24e257c8f28ee8dff1606e9c102de8a8b0f4b2da
Max-Person <max_person@mail.ru>
2023-04-26 21:50:06 +0300

    [feat] изменение поведения PredeterminingFactorsNode для отражения разницы между предрешающими и неопределенными переходами (наличие/отсуствие определяющей ветки)

--- src/its/model/nodes/PredeterminingFactorsNode.kt
+++ src/its/model/nodes/PredeterminingFactorsNode.kt
@@ -7,13 +7,14 @@ class PredeterminingFactorsNode (
     override val next: PredeterminingOutcomes
 ) : LinkNode<String>(){
     val predetermining
-        get() = next.filterKeys { it.startsWith("predetermining") }
+        get() = next.info.filter { it.decidingBranch != null }
     val undetermined
-        get() = next["undetermined"]
+        get() = next.info.singleOrNull { it.decidingBranch == null }
 
     internal constructor(el : Element) : this(
         getPredeterminingOutcomes(el)
     ){
+        require(next.info.filter { it.decidingBranch == null }.size <= 1){"У узла предрешающих факторов может быть только один неопределенный переход"}
         collectAdditionalInfo(el)
     }
 
