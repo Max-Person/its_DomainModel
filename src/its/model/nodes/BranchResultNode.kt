435622ab4394ce5a40068c179a6a5b0dd4f0d366
Max-Person <max_person@mail.ru>
2023-05-09 14:40:21 +0300

    [feat] введение выражения для дополнительных действий в BranchResultNode

--- src/its/model/nodes/BranchResultNode.kt
+++ src/its/model/nodes/BranchResultNode.kt
@@ -1,13 +1,16 @@
 package its.model.nodes
 
+import its.model.expressions.Operator
 import its.model.expressions.types.ParseValue.parseValueForBranchResult
 import its.model.nodes.visitors.DecisionTreeBehaviour
 import org.w3c.dom.Element
 
 class BranchResultNode(
-    val value: Any
+    val value: Any,
+    val actionExpr: Operator? = null,
 ) : DecisionTreeNode(){
-    internal constructor(el : Element) : this(el.getAttribute("value").parseValueForBranchResult()){
+    internal constructor(el : Element) : this(el.getAttribute("value").parseValueForBranchResult(),
+        if(el.getChild("Expression") != null) Operator.build(el.getSingleByWrapper("Expression")!!) else null){
         collectAdditionalInfo(el)
     }
 
