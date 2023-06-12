a4a74a1a323514fd9dcf97e02e3ccb5c4a3b3019
Max-Person <max_person@mail.ru>
2023-06-12 16:52:07 +0300

    [feat] возможность тривиальности для QuestionNode

--- src/its/model/nodes/QuestionNode.kt
+++ src/its/model/nodes/QuestionNode.kt
@@ -12,13 +12,15 @@ class QuestionNode (
     val type: KClass<*>,
     val isSwitch : Boolean = false,
     val expr: Operator,
-    override val next: Outcomes<Any>
+    override val next: Outcomes<Any>,
+    val trivialityExpr: Operator? = null,
 ): LinkNode<Any>() {
     internal constructor(el : Element) : this(
         typeFromString(el.getAttribute("type")).nullCheck("QuestionNode has to have a valid 'type' attribute"),
         el.getAttribute("isSwitch").toBoolean(),
         Operator.build(el.getSingleByWrapper("Expression").nullCheck("QuestionNode has to have an 'Expression' child tag")),
-        getOutcomes(el) { it.parseValue(typeFromString(el.getAttribute("type")).nullCheck("QuestionNode's outcomes' values should match its type"))}
+        getOutcomes(el) { it.parseValue(typeFromString(el.getAttribute("type")).nullCheck("QuestionNode's outcomes' values should match its type"))},
+        el.getChild("Triviality")?.let { Operator.build(it.getChild().nullCheck("Triviality must contain a valid expression XML")) },
     ){
         collectAdditionalInfo(el)
     }
@@ -26,4 +28,6 @@ class QuestionNode (
     override fun <I> use(behaviour: LinkNodeBehaviour<I>): I {
         return behaviour.process(this)
     }
+
+    fun canBeTrivial(): Boolean = isSwitch || trivialityExpr != null
 }
